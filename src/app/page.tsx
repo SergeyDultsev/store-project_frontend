65470f3239cab518c5634062efb4ec698a18d244
SergeyDultsev <121752321+Dultsev2004@users.noreply.github.com>
2025-01-22 23:31:06 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import BtnApp from "@/shared/ui/button/btnApp";
 export default function Catalog() {
     return (
         <main>
-            <BtnApp type="button" text="Получить" onClick={getNew}/>
         </main>
     );
 }
